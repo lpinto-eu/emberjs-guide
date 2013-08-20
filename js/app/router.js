2c473e283cb69c826fc33b84340abc7366c97b55
lpinto-eu <mail@lpinto.eu>
2013-08-20 23:52:17 +0100

    Adding Child Routes

--- js/app/router.js
+++ js/app/router.js
@@ -1,3 +1,13 @@
 ﻿App.Router.map(function () {
-  this.resource('todos', { path: '/' });
+    this.resource('todos', { path: '/' }, function () {
+        // additional child routes
+        });
+    });
+
+    // ... additional lines truncated for brevity ...
+
+    App.TodosIndexRoute = Ember.Route.extend({
+        model: function () {
+        return App.Todo.find();
+    }
 });
