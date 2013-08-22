3fc6456cf697fb7145f8bfe95f1e4f283a92286a
lpinto-eu <mail@lpinto.eu>
2013-08-23 00:42:21 +0100

    Replacing the Fixture Adapter with Another Adapter
    Localstorage added, but not used. Causes a bug regarding the isCompleted
    property.

--- js/app/controllers/todo.js
+++ js/app/controllers/todo.js
@@ -21,10 +21,13 @@
         var model = this.get('model');
 
         if (value === undefined) {
-            // property being used as a getter
+            
+			/* property being used as a getter */
             return model.get('isCompleted');
+
         } else {
-            // property being used as a setter
+            
+			/* property being used as a setter */
             model.set('isCompleted', value);
             model.save();
             return value;
--- js/app/models/store.js
+++ js/app/models/store.js
@@ -1,9 +1,19 @@
-﻿App.Store = DS.Store.extend({
-    adapter: 'DS.FixtureAdapter'
-});
-
+﻿﻿App.Store = DS.Store.extend({
+	adapter: 'DS.FixtureAdapter'
+ });
+ 
 App.TodosRoute = Ember.Route.extend({
     model: function () {
         return App.Todo.find();
-  }
+    }
 });
+
+/*
+App.Store = DS.Store.extend({
+    adapter: 'App.LSAdapter'
+});
+
+App.LSAdapter = DS.LSAdapter.extend({
+    namespace: 'app-emberjs'
+});
+*/
